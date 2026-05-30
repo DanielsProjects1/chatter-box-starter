65c1d3219339bea3e95f477c4e118036043e96d9
DanielsProjects1 <danielasilokun@gmail.com>
2026-05-30 19:54:48 -0400

    Added CORS to configuration

--- src/main/java/com/DanielsProjects1/Chatter_Box_Starter/config/SecurityConfig.java
+++ src/main/java/com/DanielsProjects1/Chatter_Box_Starter/config/SecurityConfig.java
@@ -14,12 +14,15 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable())
+        http
+                .cors(cors -> {})
+                .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.GET, "/api/v1/widget/*/comments").permitAll() // this allows anyone to see the comments in a box
                         .requestMatchers(HttpMethod.GET, "/api/v1/widget/*/comments/*/replies").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/v1/dashboard/sites/*/rules").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/v1/widget/init").permitAll()
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2
