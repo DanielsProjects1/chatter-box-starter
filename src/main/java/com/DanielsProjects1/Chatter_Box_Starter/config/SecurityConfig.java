4a3aaf0f7a4712157978d14f75fc1980fbfcc051
DanielsProjects1 <danielasilokun@gmail.com>
2026-05-30 21:53:23 -0400

    fix: request matcher for all options requests

--- src/main/java/com/DanielsProjects1/Chatter_Box_Starter/config/SecurityConfig.java
+++ src/main/java/com/DanielsProjects1/Chatter_Box_Starter/config/SecurityConfig.java
@@ -18,11 +18,11 @@ public class SecurityConfig {
                 .cors(cors -> {})
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/v1/widget/*/comments").permitAll() // this allows anyone to see the comments in a box
                         .requestMatchers(HttpMethod.GET, "/api/v1/widget/*/comments/*/replies").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/v1/dashboard/sites/*/rules").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/v1/widget/init").permitAll()
-                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2
