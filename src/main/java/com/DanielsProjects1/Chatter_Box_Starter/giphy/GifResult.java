dd0aedc6f1887a44f744d8e16087e30487a11689
DanielsProjects1 <danielasilokun@gmail.com>
2026-07-06 20:53:22 -0400

    Fixed GifResult field nam gifPreviewString. Changed to gifPreviewUrl to match frontend field name

--- src/main/java/com/DanielsProjects1/Chatter_Box_Starter/giphy/GifResult.java
+++ src/main/java/com/DanielsProjects1/Chatter_Box_Starter/giphy/GifResult.java
@@ -2,7 +2,7 @@ package com.DanielsProjects1.Chatter_Box_Starter.giphy;
 
 public record GifResult(
         String gifUrl,
-        String gifPreviewString,
+        String gifPreviewUrl,
         String gifProvider,
         String gifProviderId,
         String gifTitle
