d6416c57499c2f48b16af592f75c68cac469f911
DanielsProjects1 <danielasilokun@gmail.com>
2026-07-28 19:00:34 -0400

    Fixed bug in UserService regarding not yet implemented methods

--- src/main/java/com/DanielsProjects1/Chatter_Box_Starter/service/UserService.java
+++ src/main/java/com/DanielsProjects1/Chatter_Box_Starter/service/UserService.java
@@ -69,26 +69,26 @@ public class UserService {
         }
     }
 
-    @Transactional
-    public void updateUserProfile(UUID userId, String newUsername, String displayName, String pfpUrl, String bio) {
-        User user = getUserProfile(userId);
-        if (newUsername != null) user.setUsername(newUsername);
-        if (displayName != null) user.setDisplayName(displayName);
-        if (pfpUrl != null) user.setProfilePictureUrl(pfpUrl);
-        if (bio != null) user.setBio(bio);
-        userRepo.save(user);
-    }
+//    @Transactional
+//    public void updateUserProfile(UUID userId, String newUsername, String displayName, String pfpUrl, String bio) {
+//        User user = getUserProfile(userId);
+//        if (newUsername != null) user.setUsername(newUsername);
+//        if (displayName != null) user.setDisplayName(displayName);
+//        if (pfpUrl != null) user.setProfilePictureUrl(pfpUrl);
+//        if (bio != null) user.setBio(bio);
+//        userRepo.save(user);
+//    }
 
-    @Transactional
-    public void deactivateUser(UUID userId, UUID deactivatingUserId) throws AccessDeniedException {
-        User deactivator = getUserProfile(deactivatingUserId);
-        if (!userId.equals(deactivatingUserId) && deactivator.getGlobalRole() != GlobalRole.ADMIN) {
-            throw new AccessDeniedException("You cannot deactivate an account that is not your own.");
-        }
-        User user = getUserProfile(userId);
-        user.setActive(false);
-        userRepo.save(user);
-    }
+//    @Transactional
+//    public void deactivateUser(UUID userId, UUID deactivatingUserId) throws AccessDeniedException {
+//        User deactivator = getUserProfile(deactivatingUserId);
+//        if (!userId.equals(deactivatingUserId) && deactivator.getGlobalRole() != GlobalRole.ADMIN) {
+//            throw new AccessDeniedException("You cannot deactivate an account that is not your own.");
+//        }
+//        User user = getUserProfile(userId);
+//        user.setActive(false);
+//        userRepo.save(user);
+//    }
 
     @Transactional
     public User syncUser(UUID keycloakId, String email, String username) {
